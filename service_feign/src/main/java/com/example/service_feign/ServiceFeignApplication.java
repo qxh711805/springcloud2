f03d13b273db97d2f289e20db5302725af89ba2d
qxh711805 <1058626704@qq.com>
2019-03-05 23:34:44 +0800

    add service_feign

---
+++ service_feign/src/main/java/com/example/service_feign/ServiceFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFeignApplication.class, args);
+    }
+
+}
